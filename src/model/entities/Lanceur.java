f1e05b51f44e6ce9db6a020c4c4d631c9e3b7024
Mickaël Benoit <zyra83@hotmail.com>
2017-04-25 14:01:09 +0200

    Runnable
    éviter d'implémenter Runnable sur le Lievre pour éviter de mélanger les entitées avec les traitements.

--- src/model/entities/Lanceur.java
+++ src/model/entities/Lanceur.java
@@ -3,8 +3,31 @@ package model.entities;
 public class Lanceur {
 
 	public static void main(String[] args) {
-//		go();
-		goThread();
+		// go();
+		// goThread();
+		goRunnable();
+	}
+
+	private static void goRunnable() {
+		Lievre l = new Lievre();
+		Tortue t = new Tortue();
+
+//		// Classe anonyme
+//		Runnable rla = new Runnable() {
+//
+//			@Override
+//			public void run() {
+//				l.courir();
+//			}
+//		};
+
+		// Version lambda
+		Runnable rll = () -> l.courir();
+		Runnable rlt = () -> t.courir();
+		
+		new Thread(rll, "Course du lièvre").start();;
+		new Thread(rlt, "Course de la tortue").start();
+
 	}
 
 	/**
@@ -21,17 +44,17 @@ public class Lanceur {
 	private static void goThread() {
 		Thread tt = new ThreadTortue();
 		Thread tl = new ThreadLievre();
-		
+
 		// Appelle RUN dans un autre Thread
 		tt.start();
 		tl.start();
-		
+
 		// Appelle RUN dans le même Thread, c'est pas bon !!
 		// tt.run();
 		// tl.run();
-				
-		
-		// ça c'est dans le Thread principal, pas bloqué par les 2 autres Thread.
+
+		// ça c'est dans le Thread principal, pas bloqué par les 2 autres
+		// Thread.
 		System.out.println("FIN DE LA COURSE");
 		try {
 			tt.join();
@@ -39,12 +62,10 @@ public class Lanceur {
 		} catch (Exception e) {
 		}
 		System.out.println("FIN DE LA COURSE EN VRAI");
-		
+
 		System.out.println("REVANCHE");
 		tt.start();
 		tl.start();
-		
-		
 	}
 
 }
